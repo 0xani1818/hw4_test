b7ab2163b98f6ad6814247f31311b1ed31af7683
Annie <annie55079@gmail.com>
2022-12-06 07:49:13 +0800

    Initial Commit

---
+++ hw4_test/hw4_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  hw4_testApp.swift
+//  hw4_test
+//
+//  Created by Yu Zhi on 2022/12/6.
+//
+
+import SwiftUI
+
+@main
+struct hw4_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
